8e35be3b55687b7452fed786688f550cd52cb4f6
Волков Артем <x514@list.ru>
2023-01-11 20:49:56 +0300

    на проверку

--- yatube/about/tests/test_urls.py
+++ yatube/about/tests/test_urls.py
@@ -2,20 +2,21 @@ from django.test import TestCase, Client
 from http import HTTPStatus
 from django.urls import reverse
 
+
 class TestUrlAbout (TestCase):
     def test_url_exists_at_desired_location(self):
         '''Проверка доступности адреса'''
         path_list = (reverse('about:author'), reverse('about:tech'))
         for path in path_list:
-            with self.subTest(addres = path):
+            with self.subTest(addres=path):
                 respons = Client.get(path)
                 self.assertEqual(respons.status_code, HTTPStatus.OK)
 
     def test_url_uses_correct_template(self):
         """Проверка шаблона для адреса"""
-        templates_url_name = {'/author/': 'about/author.html',
+        path_template_name = {'/author/': 'about/author.html',
                               '/tech/': 'about/tech.html'}
-        for address, template in templates_url_name.items():
-            with self.subTest(address=address):
-                response = Client(address)
+        for path, template in path_template_name.items():
+            with self.subTest(address=path):
+                response = Client(path)
                 self.assertTemplateUsed(response, template)
