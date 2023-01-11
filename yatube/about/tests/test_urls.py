5af6097652697c902ab512e99a018a71de1d5a0d
Волков Артем <x514@list.ru>
2023-01-11 19:41:33 +0300

    на проверку

--- yatube/about/tests/test_urls.py
+++ yatube/about/tests/test_urls.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from http import HTTPStatus
 
 
 class TestUrlAbout (TestCase):
@@ -11,12 +12,12 @@ class TestUrlAbout (TestCase):
         for addres in url_names:
             with self.subTest(addres=addres):
                 respons = TestUrlAbout.guest_client.get(addres)
-                self.assertEqual(respons.status_code, 200)
+                self.assertEqual(respons.status_code, HTTPStatus.OK)
 
     def test_url_uses_correct_template(self):
         """Проверка шаблона для адреса"""
         templates_url_name = {'/author/': 'about/author.html',
-                              '/tech/': 'about/author.html'}
+                              '/tech/': 'about/tech.html'}
         for address, template in templates_url_name.items():
             with self.subTest(address=address):
                 response = TestUrlAbout.guest_client.get(address)
