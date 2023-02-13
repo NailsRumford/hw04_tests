be120d10a1fb36f209f0ab7bf641badace64c55f
Артем Волков <NailsRumford@internet.ru>
2023-02-13 20:51:01 +0300

    add comment test

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -2,9 +2,10 @@ from django.contrib.auth import get_user_model
 from django.test import Client
 from django.urls import reverse
 from mixer.backend.django import mixer
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from posts.tests.setting import BaseTestCase
-from posts.models import Group, Post
+from posts.models import Group, Post, Comment
 
 
 User = get_user_model()
@@ -25,6 +26,9 @@ class PostFormTest (BaseTestCase):
                                 author=self.user,
                                 group=self.group,
                                 )
+    def tearDown(self) -> None:
+        super().tearDown()
+        Post.objects.all().delete()
 
     def test_valid_form_creates_post(self):
         """
@@ -34,9 +38,24 @@ class PostFormTest (BaseTestCase):
         path = reverse('posts:post_create')
         expected_redirect_path = reverse(
             'posts:profile', args=(self.user.username,))
+        small_gif = (            
+             b'\x47\x49\x46\x38\x39\x61\x02\x00'
+             b'\x01\x00\x80\x00\x00\x00\x00\x00'
+             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+             b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+        
         form_data = {
             'text': 'test_text',
             'group': self.group.id,
+            'image': uploaded
         }
         response = self.authorized_user.post(path, data=form_data, follow=True)
         first_post_per_page = Post.objects.first()
@@ -110,3 +129,30 @@ class PostFormTest (BaseTestCase):
                                  "При отправки формы неавторизованным "
                                  "пользователем, число постов изменилось")
                 self.assertRedirects(response, expected_redirect_path)
+
+
+class CommentsFormTest (BaseTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.user = mixer.blend(User)
+        cls.authorized_user = Client()
+        cls.authorized_user.force_login(cls.user)
+        cls.goust_user = Client()
+        cls.post = mixer.blend(Post)
+        cls.path = reverse('posts:add_comment', args=(cls.post.id,))
+        
+    def test_authorized_user_can_comment_post(self):
+        comments_count = Comment.objects.count()
+        form_data= {'text': 'test text'}
+        response = self.authorized_user.post(self.path, data=form_data)
+        self.assertEqual(Comment.objects.count(),comments_count+1,
+                         'Автаризованный пользователь не может коментировать пост')
+        
+    def test_goust_user_can_comment_post(self):
+        comments_count = Comment.objects.count()
+        form_data= {'text': 'test text'}
+        response = self.goust_user.post(self.path, data=form_data)
+        self.assertNotEqual(Comment.objects.count(),comments_count+1,
+                         'Неавтаризованный пользователь может коментировать пост')
+        
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,4 +1,3 @@
-import time
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
